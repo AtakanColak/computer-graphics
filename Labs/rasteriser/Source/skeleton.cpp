8093cd740885ea275eacaf7c1765d9d2e314f402
Ata <ata@localhost.localdomain>
2019-02-28 18:01:39 +0000

    Started Rasterizer

--- Labs/rasteriser/Source/skeleton.cpp
+++ Labs/rasteriser/Source/skeleton.cpp
@@ -6,10 +6,10 @@
 #include <stdint.h>
 
 using namespace std;
-using glm::vec3;
 using glm::mat3;
-using glm::vec4;
 using glm::mat4;
+using glm::vec3;
+using glm::vec4;
 
 SDL_Event event;
 
@@ -18,41 +18,61 @@ SDL_Event event;
 #define FULLSCREEN_MODE false
 
 /* ----------------------------------------------------------------------------*/
+/* VARIABLES                                                                   */
+vec3 black(0, 0, 0);
+vec3 white(1, 1, 1);
+
+/* ----------------------------------------------------------------------------*/
 /* FUNCTIONS                                                                   */
 
 bool Update();
-void Draw(screen* screen);
+void Draw(screen *screen);
+void VertexShader(const vec4 &v, ivec2 &p);
 
-int main( int argc, char* argv[] )
+int main(int argc, char *argv[])
 {
-  
-  screen *screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE );
 
-  while ( Update())
-    {
-      Draw(screen);
-      SDL_Renderframe(screen);
-    }
+  screen *screen = InitializeSDL(SCREEN_WIDTH, SCREEN_HEIGHT, FULLSCREEN_MODE);
 
-  SDL_SaveImage( screen, "screenshot.bmp" );
+  while (Update())
+  {
+    Draw(screen);
+    SDL_Renderframe(screen);
+  }
+
+  SDL_SaveImage(screen, "screenshot.bmp");
 
   KillSDL(screen);
   return 0;
 }
 
 /*Place your drawing here*/
-void Draw(screen* screen)
+void Draw(screen *screen)
 {
   /* Clear buffer */
-  memset(screen->buffer, 0, screen->height*screen->width*sizeof(uint32_t));
-  
-  vec3 colour(1.0,0.0,0.0);
-  for(int i=0; i<1000; i++)
+  memset(screen->buffer, 0, screen->height * screen->width * sizeof(uint32_t));
+
+  for (uint32_t i = 0; i < triangles.size; ++i)
+  {
+    vector<vec4> vertices(3);
+    vertices[0] = triangles[i].v0;
+    vertices[0] = triangles[i].v1;
+    vertices[0] = triangles[i].v2;
+    for (int v = 0; v < 3; ++v)
     {
-      uint32_t x = rand() % screen->width;
-      uint32_t y = rand() % screen->height;
-      PutPixelSDL(screen, x, y, colour);
+      ivec2 projPos;
+      VertexShader(vertices[v], projPos);
+      PutPixelSDL(screen, projPos.x, projPos.y, white);
     }
+  }
+
+  // vec3 colour(1.0,0.0,0.0);
+  // for(int i=0; i<1000; i++)
+  //   {
+  //     uint32_t x = rand() % screen->width;
+  //     uint32_t y = rand() % screen->height;
+  //     PutPixelSDL(screen, x, y, colour);
+  //   }
 }
 
 /*Place updates of parameters here*/
@@ -61,39 +81,41 @@ bool Update()
   static int t = SDL_GetTicks();
   /* Compute frame time */
   int t2 = SDL_GetTicks();
-  float dt = float(t2-t);
+  float dt = float(t2 - t);
   t = t2;
 
   SDL_Event e;
-  while(SDL_PollEvent(&e))
+  while (SDL_PollEvent(&e))
+  {
+    if (e.type == SDL_QUIT)
     {
-      if (e.type == SDL_QUIT)
-	{
-	  return false;
-	}
-      else
-	if (e.type == SDL_KEYDOWN)
-	  {
-	    int key_code = e.key.keysym.sym;
-	    switch(key_code)
-	      {
-	      case SDLK_UP:
-		/* Move camera forward */
-		break;
-	      case SDLK_DOWN:
-		/* Move camera backwards */
-		break;
-	      case SDLK_LEFT:
-		/* Move camera left */
-		break;
-	      case SDLK_RIGHT:
-		/* Move camera right */
-		break;
-	      case SDLK_ESCAPE:
-		/* Move camera quit */
-		return false;
-	      }
-	  }  
+      return false;
     }
+    else if (e.type == SDL_KEYDOWN)
+    {
+      int key_code = e.key.keysym.sym;
+      switch (key_code)
+      {
+      case SDLK_UP:
+        /* Move camera forward */
+        break;
+      case SDLK_DOWN:
+        /* Move camera backwards */
+        break;
+      case SDLK_LEFT:
+        /* Move camera left */
+        break;
+      case SDLK_RIGHT:
+        /* Move camera right */
+        break;
+      case SDLK_ESCAPE:
+        /* Move camera quit */
+        return false;
+      }
+    }
+  }
   return true;
+}
+void VertexShader(const vec4& v, ivec2& p ) {
+  
 }
